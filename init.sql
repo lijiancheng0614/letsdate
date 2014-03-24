941befb82f85b0815c1d829a4e24bf0b22acb1b3
lijiancheng <lijiancheng0614@gmail.com>
2014-03-24 09:21:20 +0800

    update database sql

--- init.sql
+++ init.sql
@@ -1,36 +1,70 @@
-create table user (
-  email varchar(100) primary key,
-  name varchar(20) not null,
-  passwd char(50) not null,
-  phone varchar(30),
-  is_phone_private boolean,
-  location varchar(140),
-  is_location_private boolean,
-  intro varchar(140),
-  is_intro_private boolean
+--
+-- 数据库: `letsdate`
+--
+CREATE DATABASE IF NOT EXISTS `letsdate` DEFAULT  CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `letsdate`;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `email` varchar(100) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `passwd` char(50) NOT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `is_phone_private` tinyint(1) NOT NULL DEFAULT '0',
+  `location` text,
+  `is_location_private` tinyint(1) NOT NULL DEFAULT '0',
+  `intro` text,
+  `is_intro_private` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`email`)
 );
 
-create table date (
-  id int primary key,
-  title varchar(140) not null,
-  useremail varchar(100) not null,
-  begintime datetime not null,
-  endtime datetime,
-  location varchar(140),
-  bulletin varchar(140),
-  foreign key(useremail) references user(email),
-  index (useremail)
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `date`
+--
+
+CREATE TABLE IF NOT EXISTS `date` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `useremail` varchar(100) NOT NULL,
+  `begintime` datetime NOT NULL,
+  `endtime` datetime DEFAULT NULL,
+  `location` text,
+  `bulletin` text,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`useremail`) REFERENCES `user` (`email`),
+  KEY `useremail` (`useremail`)
 );
 
-create table datemember (
-  id int not null,
-  useremail varchar(100) not null,
-  foreign key(id) references date(id)
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `datecomment`
+--
+
+CREATE TABLE IF NOT EXISTS `datecomment` (
+  `id` int(11) NOT NULL,
+  `useremail` varchar(100) NOT NULL,
+  `comment` varchar(140) NOT NULL,
+  FOREIGN KEY (`id`) REFERENCES `date` (`id`),
+  KEY `id` (`id`)
 );
 
-create table datecomment (
-  id int not null,
-  useremail varchar(100) not null,
-  comment varchar(140) not null,
-  foreign key(id) references date(id)
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `datemember`
+--
+
+CREATE TABLE IF NOT EXISTS `datemember` (
+  `id` int(11) NOT NULL,
+  `useremail` varchar(100) NOT NULL,
+  FOREIGN KEY (`id`) REFERENCES `date` (`id`),
+  KEY `id` (`id`)
 );
